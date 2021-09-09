7d2092ade424a872de69caccca33a4b168d6d58e
zeejay <zamran.j@gmail.com>
2021-09-08 21:23:49 -0500

    Class chapter part 1

---
+++ src/excercises/classesinjavapart1/HelloWord.java
@@ -0,0 +1,12 @@
+package excercises.classesinjavapart1;
+
+public class HelloWord {
+    public String message = "Hello World";
+
+    public void sayHello(){
+        String message = "Goodbye World";
+        System.out.println(message);
+        System.out.println("");
+        System.out.println(this.message);
+    }
+}
---
+++ src/excercises/classesinjavapart1/HelloWorldRunner.java
@@ -0,0 +1,9 @@
+package excercises.classesinjavapart1;
+
+public class HelloWorldRunner {
+
+    public static void main(String[] args) {
+        HelloWord hi = new HelloWord();
+        hi.sayHello();
+    }
+}
